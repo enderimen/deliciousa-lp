57a30d6b180d407dc0d87ffe45e1a6c1772f87ed
Ender Imen <enderimen>
2020-10-17 06:06:14 +0300

    Recipe area added to project.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 export default () => {
-    console.log("Hello parcel!");
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        centeredSlides: true,
+        spaceBetween: 30,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 2000,
+        },
+        loop: true
+    });
 };
