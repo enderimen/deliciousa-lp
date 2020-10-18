d306092b41e4ae7d99a115aea273f07115f9b9ea
Ender Imen <enderimen>
2020-10-18 23:46:51 +0300

    Back to top button added to project.

--- src/main.js
+++ src/main.js
@@ -13,4 +13,28 @@ export default () => {
         },
         loop: true
     });
+
+    // Back To Top Btn
+
+    const backToTopBtn = document.querySelector("#backToTopBtn");
+    const THRESHOLD_SCROLL_VALUE = 100;
+    let currentScrollPosition;
+
+    window.onscroll = () => {
+        scrollToTop();
+    }
+
+    const scrollToTop = () => {
+        currentScrollPosition = document.documentElement.scrollTop;
+
+        if(backToTopBtn && currentScrollPosition > THRESHOLD_SCROLL_VALUE) {
+            backToTopBtn.classList.add("-show");
+        } else {
+            backToTopBtn.classList.remove("-show");
+        }
+    }
+
+    backToTopBtn.addEventListener('click',() => {
+        currentScrollPosition = 0;
+    });
 };
